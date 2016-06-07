4dcbd523fb639e979895a17e0c9c42a6319115cc
Tory BYU <torys.anderson@gmail.com>
2016-06-06 18:36:05 -0600

    cleanup gen files

--- target/cljsbuild/public/js/out/humfunding/validation.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.8.40 {:static-fns true, :optimize-constants true}
-goog.provide('humfunding.validation');
-goog.require('cljs.core');
-goog.require('bouncer.core');
-goog.require('bouncer.validators');
--- target/cljsbuild/public/js/out/reagent/debug.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.8.40 {:static-fns true, :optimize-constants true}
-goog.provide('reagent.debug');
-goog.require('cljs.core');
